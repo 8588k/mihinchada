ce854adc8c5f9c28463525e9f6595f229ffaa9d5
Matías M. Méndez <matimendez88@gmail.com>
2015-12-08 16:07:09 -0300

    admob module added

---
+++ client/www/app/admob.module.js
@@ -0,0 +1,38 @@
+App.module('Vamo', function (Vamo, App, Backbone, Marionette, $, _) {
+    
+    Vamo.AdMob = Marionette.Object.extend({
+        ids: {},
+
+        lastShow: 0,
+
+        betweenShowDifference: 1000 * 60 * 5,
+
+        initialize: function(options){
+            if( /(android)/i.test(navigator.userAgent) ) { 
+                this.ids = { // for Android
+                    // interstitial: 'ca-app-pub-5913386662735594/6263217463'
+                };
+            } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+                this.ids = { // for iOS
+                    // interstitial: 'ca-app-pub-6869992474017983/7563979554'
+                };
+            } else {
+                this.ids = { // for Windows Phone
+                    // interstitial: 'ca-app-pub-6869992474017983/1355127956'
+                };
+            }
+        },
+
+        showInterstitial: function(){
+            var now = new Date().getTime();
+            if((now - this.lastShow) > this.betweenShowDifference){
+                AdMob.prepareInterstitial({
+                    adId: this.ids.interstitial,
+                    autoShow: true
+                });
+
+                this.lastShow = now;
+            }
+        }
+    });
+});
