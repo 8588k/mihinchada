9c5a5afa972ee3ab655e93f36914d4c9c7bc1a37
Matías M. Méndez <matimendez88@gmail.com>
2015-12-08 16:03:41 -0300

    miHinchada module added

---
+++ client/www/app/miHinchada.module.js
@@ -0,0 +1,33 @@
+App.module('miHinchada', function (miHinchada, App, Backbone, Marionette, $, _) {
+    var controller,
+        adds,
+        Router;
+
+    Router = Marionette.AppRouter.extend({
+        'appRoutes': {
+            '': 'index'
+        }
+    });
+
+    controller = {
+        index: function() {
+            var mainLayoutView;
+
+            mainLayoutView = new miHinchada.Views.Main();
+            App.mainRegion.show(mainLayoutView);
+        }
+    };
+
+    App.onStart = function() {
+        new Router({
+            controller: controller
+        });
+
+        adds = new miHinchada.AdMob();
+        adds.showInterstitial();
+    };
+
+    App.onResume = function() {
+        adds.showInterstitial();
+    };
+});
