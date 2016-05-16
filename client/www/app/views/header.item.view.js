4e0b3bcb41971227b090c17dd7d92d12de8b36a3
Matías M. Méndez <matimendez88@gmail.com>
2016-05-15 21:24:40 -0300

    header added

---
+++ client/www/app/views/header.item.view.js
@@ -0,0 +1,38 @@
+App.module('miHinchada.Views', function (Views, App, Backbone, Marionette, $, _) {    
+    
+    Views.Header = Marionette.ItemView.extend({
+
+        template: __templates.mihinchada.header
+
+        // ui: {
+        //     share: '[data-js="share"]',
+        //     reset: '[data-js="reset"]'
+        // },
+
+        // events: {
+        //     'touchstart @ui.options': 'showOptions',
+        //     'touchstart @ui.share': 'share',
+        //     'touchstart @ui.reset': 'reset'
+        // },
+
+        // onShow: function() {
+            // $('.dropdown-button').dropdown({
+            //     inDuration: 300,
+            //     outDuration: 225,
+            //     constrain_width: false, // Does not change width of dropdown to that of the activator
+            //     hover: false, // Activate on hover
+            //     gutter: 0, // Spacing from edge
+            //     belowOrigin: true, // Displays dropdown below the button
+            //     alignment: 'left' // Displays dropdown with edge aligned to the left of button
+            // });
+        // },
+
+        // share: function(event){
+        //     App.share();
+        // },
+
+        // reset: function(event){
+        //     Backbone.history.navigate('reset', {trigger:true});
+        // }
+    });
+});
