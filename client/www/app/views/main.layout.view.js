0b54a872feefa74d15b398aaca7728f7e7185d7c
Matías M. Méndez <matimendez88@gmail.com>
2016-05-15 21:22:20 -0300

    main added

--- client/www/app/views/main.layout.view.js
+++ client/www/app/views/main.layout.view.js
@@ -2,17 +2,26 @@ App.module('miHinchada.Views', function (Views, App, Backbone, Marionette, $, _)
 
     Views.Main = Marionette.LayoutView.extend({
 
+        className: 'main-layout',
+
         template: __templates.mihinchada.main,
 
         regions: {
+            'headerRegion': '[data-js="header"]',
+            'tabsRegion': '[data-js="tabs"]',
             'contentRegion': '[data-js="content"]'
         },
 
-        events: {},
-
         onShow: function(options) {
-            var that = this,
-                match;
+            var headerItemView,
+                tabsItemView,
+                matchLayoutView;
+
+            headerItemView = new Views.Header();
+            this.headerRegion.show(headerItemView);
+
+            tabsItemView = new Views.Tabs();
+            this.tabsRegion.show(tabsItemView);
 
             matchLayoutView = new Views.Match();
             this.contentRegion.show(matchLayoutView);
